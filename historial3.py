fc92181dfb474b485b5f7278c493f7e63843feb9
sidvicius96 <72944063+sidvicius96@users.noreply.github.com>
2022-08-11 15:16:25 -0300

    Add files via upload

---
+++ historial3.py
@@ -0,0 +1,10 @@
+
+historial3=(   9530, 4120, 4580, 1500, 890,7516,426)
+historial3
+(9530, 4120, 4580, 1500, 890, 7516, 426)
+prom=sum (historial3)/len(historial3)
+print("el promerdio de las secciones de Lennon es",prom)
+if prom > 4500:
+  print("Se ha excedido del gasto promedio para su mascota")
+
+
